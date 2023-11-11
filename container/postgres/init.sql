a1ba607e7e2297970ae539d044d5086bcc751eb5
depq <lap14646@depq-2.local>
2023-11-11 15:42:14 +0700

    gen orders data

--- container/postgres/init.sql
+++ container/postgres/init.sql
@@ -20,8 +20,34 @@ CREATE TABLE users (
     PASSWORD VARCHAR(255) NOT NULL
 );
 
+-- create an orders table
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    product_id INT REFERENCES products(id),
+    quantity INT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
 ALTER TABLE
     products REPLICA IDENTITY FULL;
 
 ALTER TABLE
     users REPLICA IDENTITY FULL;
+
+-- Set REPLICA IDENTITY to FULL for orders table
+ALTER TABLE orders REPLICA IDENTITY FULL;
+
+
+
+create schema revenue_report;
+SET
+    search_path TO revenue_report;
+
+
+CREATE TABLE report(
+    date_id varchar(8) primary key,
+    revenue real
+);
--- datagen/gen_user_payment_data.py
+++ datagen/gen_user_payment_data.py
@@ -7,9 +7,39 @@ from faker import Faker
 fake = Faker()
 
 
-def gen_user_product_data(num_records: int) -> None:
-    for id in range(num_records):
-        sleep(0.5)
+def get_key_for_gen_data() -> int:
+    try:
+        # Establish a connection to the PostgreSQL database
+        conn = psycopg2.connect(
+            "dbname='postgres' user='postgres' host='postgres_custom' password='postgres'"
+        )
+
+        # Create a cursor
+        curr = conn.cursor()
+
+        # Execute the SQL query to get the count of records in the users table
+        curr.execute("SELECT count(*) FROM commerce.users")
+
+        # Fetch the result
+        count = curr.fetchone()[0]
+
+        # Close the cursor and the connection
+        curr.close()
+        conn.close()
+
+        # Return the count
+        return count
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0  # Return 1 to indicate an error
+
+
+def gen_data(num_records: int) -> None:
+    # how to handel if connection refuse
+    start = get_key_for_gen_data()
+    for id in range(start+1, num_records):
+        sleep(1)
         conn = psycopg2.connect(
             "dbname='postgres' user='postgres' host='postgres_custom' password='postgres'"
         )
@@ -21,6 +51,10 @@ def gen_user_product_data(num_records: int) -> None:
         curr.execute(
             "INSERT INTO commerce.products (id, name, description, price) VALUES (%s, %s, %s, %s)",
             (id, fake.name(), fake.text(), fake.random_int(min=1, max=100)),
+        )
+        curr.execute(
+            "INSERT INTO commerce.orders (id, user_id, product_id, quantity, order_date) VALUES (%s, %s, %s, %s, %s)",
+            (id, id, id, fake.random_int(min=1, max=10), fake.date_this_month()),
         )
         conn.commit()
 
@@ -37,11 +71,11 @@ def gen_user_product_data(num_records: int) -> None:
             )
         conn.commit()
 
-        sleep(0.5)
-        # delete 5 % of the time
-        if random.randint(1, 100) >= 95:
-            curr.execute("DELETE FROM commerce.users WHERE id = %s", (id,))
-            curr.execute("DELETE FROM commerce.products WHERE id = %s", (id,))
+        # sleep(0.5)
+        # # delete 5 % of the time
+        # if random.randint(1, 100) >= 95:
+        #     curr.execute("DELETE FROM commerce.users WHERE id = %s", (id,))
+        #     curr.execute("DELETE FROM commerce.products WHERE id = %s", (id,))
 
         conn.commit()
         curr.close()
@@ -60,4 +94,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     num_records = args.num_records
-    gen_user_product_data(num_records)
+    gen_data(num_records)
